aae34881541255272c7d12b68db2031c6e1814d0
TMY-GH <prog0323fujii@gmail.com>
2021-07-07 12:47:49 +0900

    新規登録画面まで作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,34 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+          :authentication_keys => [:user_name]
 
 # --- バリデーション ---
   with_options presence: true do
     validates :nick_name
-    validates :user_name
+    validates :user_name, format: { with: /\A[a-zA-Z0-9]+\z/ }, uniqueness: true
+  end
+
+
+
+
+#usernameを利用してログインするようにオーバーライド
+def self.find_first_by_auth_conditions(warden_conditions)
+  conditions = warden_conditions.dup
+  if login = conditions.delete(:login)
+    #認証の条件式を変更する
+    where(conditions).where(["username = :value", { :value => user_name }]).first
+  else
+    where(conditions).first
+  end
+end
+# メールアドレスを使わない設定
+  def email_required?
+    false
+  end
+  def email_changed?
+    false
+  end
+  def will_save_change_to_email?
+    false
   end
 end
