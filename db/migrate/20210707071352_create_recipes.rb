9dbb6785c7e604da6b1e11efa5ae205b4084640f
TMY-GH <prog0323fujii@gmail.com>
2021-07-07 16:18:46 +0900

    recipeモデルの作成

---
+++ db/migrate/20210707071352_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name,         null: false
+      t.integer :time,        null: false
+      t.integer :serving,     null: false
+      t.integer :publish_id,  null: false
+      t.integer :price,       null: false
+      t.references :user,     null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_012252) do
+ActiveRecord::Schema.define(version: 2021_07_07_071352) do
+
+  create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "time", null: false
+    t.integer "serving", null: false
+    t.integer "publish_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nick_name", null: false
@@ -24,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_07_07_012252) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipes", "users"
 end
