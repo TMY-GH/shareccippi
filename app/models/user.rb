8d30675a28e245a4b0088a2adca8fe789a2a1cdd
TMY-GH <prog0323fujii@gmail.com>
2021-07-07 10:34:22 +0900

    バリデーションとストロングパラメーター

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+# --- バリデーション ---
+  with_options presence: true do
+    validates :nick_name
+    validates :user_name
+  end
 end
