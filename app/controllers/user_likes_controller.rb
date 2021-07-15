bfeb42d3729a5c06b0451233064c51ce48907f6a
TMY-GH <prog0323fujii@gmail.com>
2021-07-15 15:56:07 +0900

    アクションの詳細の記載

--- app/controllers/user_likes_controller.rb
+++ app/controllers/user_likes_controller.rb
@@ -1,9 +1,14 @@
 class UserLikesController < ApplicationController
-  
+  before_action :authenticate_user!
+
   def create
+    like = current_user.favorites.create(recipe_id: params[:recipe_id])
+    redirect_to root_path
   end
 
   def destroy
+    like = UserLike.find(user_id: current_user.id, recipe_id: params[:redipe_id])
+    like.destroy
+    redirect_to root_path
   end
-  
 end
--- app/models/user_like.rb
+++ app/models/user_like.rb
@@ -2,4 +2,6 @@ class UserLike < ApplicationRecord
   # Association
   belongs_to :user
   belongs_to :recipe
+  # ユーザーIDとレシピIDが同じ組み合わせは1つのみ
+  validates_uniqueness_of :recipe_id, scope: :user_id
 end
