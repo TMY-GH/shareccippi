44757ecd1062b6936e80c259517b69ed6759cbdc
TMY-GH <prog0323fujii@gmail.com>
2021-07-15 21:47:12 +0900

    星を押すときにクリック感が出るように変更

--- app/javascript/add_like.js
+++ app/javascript/add_like.js
@@ -1,3 +1,19 @@
+const like_animation = () => {
+  if (document.getElementsByClassName("like-button")){
+    let like_btn = document.getElementsByClassName("like-button");
+    like_btn = like_btn[0];
+    like_btn.addEventListener('mousedown', () =>{
+      like_btn.style.fontSize = "79px";
+    });
+    like_btn.addEventListener('mouseup', () =>{
+      like_btn.style.fontSize = "80px";
+    });
+  };
+};
+
+window.addEventListener('load', like_animation);
+
+
 // いいねの非同期通信作りましたが、remote: trueするだけでよかったみたいです
 
 // const add_like = () => {
