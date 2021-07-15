36b23834113b0360515f616340d6857cc0f9edc0
TMY-GH <prog0323fujii@gmail.com>
2021-07-15 16:20:44 +0900

    お気に入り機能の実装

--- app/controllers/user_likes_controller.rb
+++ app/controllers/user_likes_controller.rb
@@ -2,12 +2,12 @@ class UserLikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    like = current_user.favorites.create(recipe_id: params[:recipe_id])
+    like = current_user.user_likes.create(recipe_id: params[:recipe_id])
     redirect_to root_path
   end
 
   def destroy
-    like = UserLike.find(user_id: current_user.id, recipe_id: params[:redipe_id])
+    like = UserLike.find_by(user_id: current_user.id, recipe_id: params[:recipe_id])
     like.destroy
     redirect_to root_path
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,21 +17,26 @@ class User < ApplicationRecord
 
   validates :user_name, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-zA-Z\d]{4,16}\z/i, message: "が正しくありません"}
 
-
-# Remember_meを常にTrue
-def remember_me
-  true
-end
-#usernameを利用してログインするようにオーバーライド
-def self.find_first_by_auth_conditions(warden_conditions)
-  conditions = warden_conditions.dup
-  if login = conditions.delete(:login)
-    #認証の条件式を変更する
-    where(conditions).where(["username = :value", { :value => user_name }]).first
-  else
-    where(conditions).first
+# --- Method ---
+  # お気に入り済みか判断
+  def already_liked?(recipe)
+    self.user_likes.exists?(recipe_id: recipe.id)
   end
-end
+
+  # Remember_meを常にTrue
+  def remember_me
+    true
+  end
+  #usernameを利用してログインするようにオーバーライド
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      #認証の条件式を変更する
+      where(conditions).where(["username = :value", { :value => user_name }]).first
+    else
+      where(conditions).first
+    end
+  end
 # メールアドレスを使わない設定
   def email_required?
     false
