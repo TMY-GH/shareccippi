4a9e8eeeb91b2706835e5152ae791e814b4c09f1
TMY-GH <prog0323fujii@gmail.com>
2021-07-18 20:57:39 +0900

    turbolinksの停止

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,16 +4,16 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../add_ingredient")
 require("../add_cooking")
-require("destroy_modal")
-require("search_ingredients")
-require("animation_like")
-require("preview")
-require("select_publish")
+require("../destroy_modal")
+require("../search_ingredients")
+require("../animation_like")
+require("../preview")
+require("../select_publish")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
