2f8abe5a6ccd14ced95e036c885263568cb63e05
TMY-GH <prog0323fujii@gmail.com>
2021-07-23 12:38:45 +0900

    バリデーションの実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_group, only: [:edit, :update, :destroy]
   
   def index
     user = User.find(current_user.id)
@@ -21,9 +22,22 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+    @group_form = GroupForm.new(group_name: @group.name)
+  end
+
+  def update
+    @group_form = GroupForm.new(group_form_params_update)
+    if @group_form.valid?
+      @group_form.update
+      redirect_to groups_path
+    else
+      render :edit
+    end
+  end
+
   def destroy
-    group = Group.find(params[:id])
-    group.destroy
+    @group.destroy
     redirect_to groups_path
   end
 
@@ -31,5 +45,11 @@ class GroupsController < ApplicationController
   def group_form_params
     params.require(:group_form).permit(:group_name, user_names: []).merge(current_user_id: current_user.id)
   end
+  def group_form_params_update
+    params.require(:group_form).permit(:group_name, user_names: []).merge(current_user_id: current_user.id, group_id: params[:id])
+  end
 
+  def set_group
+    @group = Group.find(params[:id])
+  end
 end
--- app/models/group_form.rb
+++ app/models/group_form.rb
@@ -3,15 +3,15 @@ class GroupForm
   include ActiveModel::Validations
   include ActiveModel::Attributes
 
-  attr_accessor :group_name, :user_names, :current_user_id
+  attr_accessor :group_name, :user_names, :current_user_id, :group_id
 
 # Validation
   with_options presence: true do
     validates :group_name
-    validates :user_names
+    validates :user_names, if: :blank?
   end
   # 存在しないユーザーID && 空の保存ができない（配列版）
-  validates :user_names, invite_user: true
+  validates :user_names, invite_user: { attr: 'group_id' }
 
 # Method
   def save
@@ -22,7 +22,17 @@ class GroupForm
       user = User.find_by(user_name: user_name)
       GroupInvitation.create(user_id: user.id, group_id: group.id)
     end
+  end
 
+  def update
+    group = Group.find(group_id)
+    group.update(name: group_name)
+    unless user_names.blank?
+      user_names.each do |user_name|
+        user = User.find_by(user_name: user_name)
+        GroupInvitation.create(user_id: user.id, group_id: group.id)
+      end
+    end
   end
 
 end
