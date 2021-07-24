d69d4c9786b702ab6dc3e6243e926d5d79413e3d
TMY-GH <prog0323fujii@gmail.com>
2021-07-24 15:11:49 +0900

    バリデーションの修正

--- app/validators/invite_user_validator.rb
+++ app/validators/invite_user_validator.rb
@@ -3,19 +3,20 @@ class InviteUserValidator < ActiveModel::EachValidator
     if values == nil || values == ""
       return
     end
-    i = 1
+    i = 0
     values.each do |value|
+      i += 1
       if value.blank?
         record.errors[attribute] << (options[:message] || "（#{i}つ目）を入力してください")
       end
       unless User.find_by(user_name: value)
         record.errors[attribute] << (options[:message] || "（#{i}つ目）は存在しません")
+        break
       end
       user = User.find_by(user_name: value)
       if GroupInvitation.find_by(user_id: user.id, group_id: record.group_id) || UserGroup.find_by(user_id: user.id, group_id: record.group_id)
         record.errors[attribute] << (options[:message] || "（#{i}つ目）は既に招待しています")
       end
-      i += 1
     end
   end
 end
