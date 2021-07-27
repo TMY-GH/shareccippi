9240e2b84253d6c475fa5f75872705ef3a71581f
TMY-GH <prog0323fujii@gmail.com>
2021-07-27 14:12:27 +0900

    チェックボックスの見た目実装

--- app/controllers/group_likes_controller.rb
+++ app/controllers/group_likes_controller.rb
@@ -1,9 +1,13 @@
 class GroupLikesController < ApplicationController
 
   def new
+    @group = Group.find(params[:group_id])
+    @recipes = current_user.recipes.order("created_at DESC")
+    @group_like = GroupLike.new
   end
 
   def create
+    binding.pry
   end
 
   def edit
