f608ba40d7f8a06d3d460cd23f9bc5425c05699c
TMY-GH <prog0323fujii@gmail.com>
2021-07-30 18:44:31 +0900

    アソシエーション

--- app/models/group_shopping.rb
+++ app/models/group_shopping.rb
@@ -1,2 +1,4 @@
 class GroupShopping < ApplicationRecord
+  has_many :group_shopping_lists
+  has_many :recipe_ingredients, through: :group_shopping_lists  
 end
--- app/models/group_shopping_list.rb
+++ app/models/group_shopping_list.rb
@@ -1,2 +1,4 @@
 class GroupShoppingList < ApplicationRecord
+  belongs_to :group_shopping
+  belongs_to :recipe_ingredient
 end
